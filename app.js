2012bbd13426de93ae29e3f919c57406a9dc1c41
Paschal A. Ogba <paxbond01@gmail.com>
2023-07-03 05:50:27 +0100

    server initial test

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app  = express();
 
+const port = process.env.port || 3000;
+
+
+app.get('/', (req, res)=>{
+    console.log('from console----> home route');
+    return res.send('<h1>Home Route</h1>');
+});
+
+
+app.listen(port, ()=>{
+    console.log(`app runnning on http://localhost:${port}`);
+});
